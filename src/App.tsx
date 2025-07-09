9144e29d115e50e755bcaa513fc8544bb582a66e
Aleksa Radovic <aleksaradovic2000@gmail.com>
2025-07-09 13:47:39 +0200

    Implement Category section and horizontal scroll buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+import { CategoryPills } from "./components/CategoryPills"
 import { PageHeader } from "./components/PageHeader"
+import { categories } from "./data/home"
 function App() {
+  const [selectedCategry, setSelectedCategory] = useState(categories[0])
   return <div className="max-h-screen flex flex-col">
     <PageHeader/>
+    <div className="grid  flex-grow-1 overflow-auto" 
+    style={{'gridTemplateColumns': 'auto 1fr'}}>
+      <div>Sidebar</div>
+      <div className="overflow-x-hidden px-8 pb-4">
+        <div className="sticky top-0 bg-white-500 z-10 pb-4">
+          <CategoryPills categories={categories}
+          selectedCategory={selectedCategry} onSelect={setSelectedCategory} />
+        </div>
+      </div>
+    </div>
   </div>
 }
 
